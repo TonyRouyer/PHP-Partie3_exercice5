5cd4ced570552775e8af88f644336bae8a001c3a
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-10 12:07:30 +0200

    correction

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>partie 3 exercice 5</title>
 </head>
 <body>
     <?php
         for($i = 0; $i <= 15; $i++){
-            echo '<p>On y arrive presque, deja ' . $i . ' pas</p>';
+            ?><p><?= 'On y arrive presque, deja ' . $i . ' pas' ?></p><?php
         }
     ?>
 </body>
